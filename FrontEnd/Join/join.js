fbe717a155f754669fa9b7dd865ce19709308b48
Walid Jeghnine <jeghninewalid@outlook.fr>
2023-08-04 15:43:47 +0200

    variable modification

--- FrontEnd/Join/join.js
+++ FrontEnd/Join/join.js
@@ -12,13 +12,13 @@ try {
     .then(json => {
         const tab = json[0].date.split("T");
         const tab3 = tab[0].split("-").reverse();
-        const str3 = tab3[0] + "/" + tab3[1] + "/" + tab3[2]; 
+        const str2 = tab3.join("/");
         const tab2 = tab[1].split(":");
-        const str2 = tab2[0] + "h" + tab2[1];
+        const str = tab2[0] + "h" + tab2[1];
         title.innerHTML += `<p>${json[0].title}</p>`;
         place.innerHTML += `<p>${json[0].place}</p>`;
-        date.innerHTML += `<p>${str3}</p>`;
-        time.innerHTML += `<p>${str2}</p>`;
+        date.innerHTML += `<p>${str2}</p>`;
+        time.innerHTML += `<p>${str}</p>`;
     });
 
 } catch (error) {
