07c429863718d7a79402410c7dfde1d65a15a893
Walid Jeghnine <jeghninewalid@outlook.fr>
2023-08-04 15:41:13 +0200

    connecting front to back

--- FrontEnd/Create/create.js
+++ FrontEnd/Create/create.js
@@ -1,13 +1,31 @@
 const title = document.getElementById("text");
+const place = document.querySelector(".place>input");
+const date = document.querySelector(".date-time>input")
 const button = document.querySelector(".my-ipt");
 
-button.addEventListener("click",() => {
-    fetch("http://localhost:3000/event/create", {
-
-        method: "POST",
+button.addEventListener("click",(event) => {
+    
+    event.preventDefault()
+    console.log(title.value, place.value, date.value);
+    
+    try {
         
-        body: JSON.stringify({
-            title,
-        })
-    })
+        fetch("http://localhost:3000/event/create", {
+    
+            method: "POST",
+            
+            body: JSON.stringify({
+                title: title.value,
+                place: place.value,
+                date: date.value
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }).then(response => response.json())
+ 
+        .then(json => console.log(json));
+    } catch (error) {
+        console.log(error.message);
+    }
 })
--- FrontEnd/Join/join.js
+++ FrontEnd/Join/join.js
@@ -1,13 +1,39 @@
 const button = document.querySelector("button");
 const input = document.querySelector(".input");
+const title = document.querySelector(".title");
+const place = document.querySelector(".place")
+const date = document.querySelector(".date")
+const time = document.querySelector(".time")
+
+try {
+
+    fetch("http://localhost:3000/events" )
+    .then(response => response.json())
+    .then(json => {
+        const tab = json[0].date.split("T");
+        const tab3 = tab[0].split("-").reverse();
+        const str3 = tab3[0] + "/" + tab3[1] + "/" + tab3[2]; 
+        const tab2 = tab[1].split(":");
+        const str2 = tab2[0] + "h" + tab2[1];
+        title.innerHTML += `<p>${json[0].title}</p>`;
+        place.innerHTML += `<p>${json[0].place}</p>`;
+        date.innerHTML += `<p>${str3}</p>`;
+        time.innerHTML += `<p>${str2}</p>`;
+    });
+
+} catch (error) {
+    
+}
 
 button.addEventListener("click", () =>{
     if(button.innerText === 'Je participe'){
         button.style.backgroundColor = 'red'
         button.innerText = 'Je ne participe pas'
+        input.classList.add('hide')
     }else if(button.innerText === 'Je ne participe pas'){
         button.style.backgroundColor = 'rgb(69, 177, 230)'
         button.innerText = 'Je participe'
+        input.classList.remove('hide')
     }
 });
 
@@ -18,3 +44,4 @@ const enter = (event) =>{
 }
 
 input.addEventListener("keypress", enter)
+
